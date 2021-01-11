44f27e712932a0efa64911582ad50bd21cbbf1e6
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-01-11 15:34:56 -0600

    Add files via upload

---
+++ js-rocket/js/script.js
@@ -0,0 +1,30 @@
+var car = {
+			make: 'VW',
+			type: 'Thing',
+			color: 'Sandal Brown',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: [
+				'seat 1',
+				'seat 2', 
+				'seat 3', 
+				'seat 4'
+			],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {
+				console.log('turn car' +isOn)
+					;
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				} else {
+					this.isTurnedOn = false;
+				}
+			},		
+		};
+
+		console.log("Hello there friends!");
